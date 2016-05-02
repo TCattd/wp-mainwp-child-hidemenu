89b682a925c8a702c1184f09e71303a145b81ede
Esteban <esteban.git@actitud.xyz>
2016-05-02 18:14:28 -0300

    MainWP Child 3.1.x

--- wp-mainwp-child-hidemenu.php
+++ wp-mainwp-child-hidemenu.php
@@ -3,12 +3,12 @@
 Plugin Name: MainWP Child: hide menu
 Plugin URI: https://github.com/TCattd/wp-mainwp-child-hidemenu
 Description: Hides MainWP's child menu item from clients
-Version: 1.0.0
+Version: 1.0.1
 Author: Esteban Cuevas
 Author URI: http://www.attitude.cl
 License: GPLv2
 License URI: http://opensource.org/licenses/GPL-2.0
-*/
+ */
 
 function tcattd_mainwp_child_hidemenu() {
 	global $current_user;
@@ -17,10 +17,11 @@ function tcattd_mainwp_child_hidemenu() {
 
 	get_currentuserinfo();
 
-	if(strtolower($current_user->user_login) != 'killtro' AND strtolower($current_user->user_login) != 'esteban' AND strtolower($current_user->user_login) != 'admin' OR $forceit === true)
-	{
+	if (strtolower($current_user->user_login) != 'killtro' AND strtolower($current_user->user_login) != 'esteban' AND strtolower($current_user->user_login) != 'admin' OR $forceit === true) {
 
 		remove_menu_page('mainwp_child_tab');
+		remove_submenu_page('options-general.php', 'mainwp_child_tab'); //3.1.x
+		remove_submenu_page('admin.php', 'mainwp_child_tab'); //3.1.x
 
 	}
 }
